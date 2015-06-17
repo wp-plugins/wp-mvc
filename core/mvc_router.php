59e0b401a86d2b34b86ce0a8090c5206e23722c3
robert.peake <robert.peake@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 09:07:25 +0000

    fixed issue with command-line plugin generator, added custom dashicons
    git-svn-id: https://plugins.svn.wordpress.org/wp-mvc/trunk@1182439 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/mvc_router.php
+++ core/mvc_router.php
@@ -26,7 +26,7 @@ class MvcRouter {
             }
             $model = MvcModelRegistry::get_model($model_name);
             if (!empty($model) && method_exists($model, 'to_url')) {
-                $url = site_url('/');
+                $url = home_url('/');
                 $method = new ReflectionMethod(get_class($model), 'to_url');
                 $parameter_count = $method->getNumberOfParameters();
                 if ($parameter_count == 2) {
@@ -49,7 +49,7 @@ class MvcRouter {
                 }
             }
         }
-        $url = site_url('/');
+        $url = home_url('/');
         if ($matched_route) {
             $path_pattern = $matched_route[0];
             preg_match_all('/{:([\w]+).*?}/', $path_pattern, $matches, PREG_SET_ORDER);
@@ -114,7 +114,7 @@ class MvcRouter {
         return $params;
     }
     
-    public function admin_page_param($options=array()) {
+    static function admin_page_param($options=array()) {
         if (is_string($options)) {
             $options = array('model' => $options);
         }
